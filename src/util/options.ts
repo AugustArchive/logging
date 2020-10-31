53511a085994ed31cc156f1667839dd2f90156d8
Noel <august@augu.dev>
2020-10-31 05:14:20 -0700

    2.0.0 rework

--- src/util/options.ts
+++
@@ -1,9 +0,0 @@
-export default function getOption<T>(prop: string, defaultValue: T, options?: any): T {
-  // If options is defined,
-  //   check if the property exists
-  //       if so, return the value
-  //       else use the default value provides
-  //   if not, use the default value
-  // return the default value if the options object doesn't exist
-  return options ? options.hasOwnProperty(prop) ? options[prop]! : defaultValue : defaultValue;
-}
